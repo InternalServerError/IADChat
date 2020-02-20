27d26f4a7c31d16573451acb9e0c015d0254b861
InternalServerError <zilbermann.cyril@gmail.com>
2020-02-20 05:18:10 -0800

    Add Dispatcher and Router classes skeleton

---
+++ app/src/Core/Dispatcher.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Foo\Core;
+
+use Foo\App\Controller\Controller as Controller;
+
+class Dispatcher 
+{
+    private $request;
+
+    /**
+     * Dispatch request to router
+     */
+    public function dispatch()
+    {
+    }
+}
